102538d1bd68275081d0d6ab18988475e5dfe52d
rathish476 <72910651+rathish476@users.noreply.github.com>
2020-10-15 14:12:06 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,13 @@
+class commandline{
+
+
+public static void main(String []args){
+
+System.out.println("Command-Line arguments are 123");
+for(String str:args){
+System.out.println(str);
+} 
+
+}
+
+}
