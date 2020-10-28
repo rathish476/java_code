175d50d885e7c8e09eafe80b7ddd3f794fbea19d
rathish476 <72910651+rathish476@users.noreply.github.com>
2020-10-28 13:23:16 +0530

    Add files via upload

---
+++ DecrementOperatorDemo.java
@@ -0,0 +1,18 @@
+class DecrementOperatorDemo{
+
+public static void main(String args[]){
+
+int x = 131;
+int y = 124;
+int z = --x;
+
+System.out.println(z--);
+System.out.println(--x);
+System.out.println(y--);
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+}
+
+}
