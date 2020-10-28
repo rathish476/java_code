57589b5c7cefac1f59b7873f51e7c3c6d95cd676
rathish476 <72910651+rathish476@users.noreply.github.com>
2020-10-28 13:54:01 +0530

    Add files via upload

---
+++ BitwiseOperatorDemo.java
@@ -0,0 +1,11 @@
+class BitwiseOperatorDemo{
+public static void main (String[] args){
+int x = 10 , y = 20;
+
+System.out.println("(x==y) & (x < y):"+((x==y)&(x++<y)));
+System.out.println("x:"+x);
+System.out.println("(x==y)|(x < y):"+((x==y)|(x++< y)));
+System.out.println("x :"+ x);
+}
+
+}
