5aae2f03495a6427c7e7a6368e617f4dfcfaa747
rathish476 <72910651+rathish476@users.noreply.github.com>
2020-10-28 14:17:24 +0530

    Add files via upload

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,14 @@
+public class RelationalOperatorDemo{
+
+   public static void main(String args[]) {
+      int x = 10;
+      int y = 20;
+
+      System.out.println("x == y = " + (x == y) );
+      System.out.println("x != y = " + (x != y) );
+      System.out.println("x > y = " + (x > y) );
+      System.out.println("x < y = " + (x < y) );
+      System.out.println("y >= x = " + (y >= x) );
+      System.out.println("y <= x = " + (y <= x) );
+   }
+}
