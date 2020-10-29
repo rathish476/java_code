0a564cbeda9f91bf846adff09a1882df464223ca
rathish476 <72910651+rathish476@users.noreply.github.com>
2020-10-29 08:49:25 +0530

    Add files via upload

---
+++ ContinueStatementDemo.java
@@ -0,0 +1,21 @@
+class ContinueStatementDemo
+{
+ public static void main(String args[])
+{
+ 
+ System.out.println("even numbers between 1-10");
+
+ for(int i=0 ; i<10 ; i++)
+{
+    if ( i%2 == 1)
+{
+           continue;
+
+}
+    
+    System.out.println(i);
+}
+
+}
+
+}
